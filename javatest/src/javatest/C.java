a790a646d96a7313b09b0bf31502190ec4b2f9e3
Chandu <mailmealluri2499@gmail.com>
2021-02-19 17:32:29 +0530

    modified A, added class C

--- javatest/src/javatest/C.java
+++ javatest/src/javatest/C.java
@@ -2,11 +2,11 @@ package javatest;
 
 public class C {
 
-	static int a=20;
+	static int a=10;
 	static int b=30;
 	
 	
-	// this is main method
+	// this is main method in C
 	public static void main(String[] args) {
 		System.out.println(a+b);
 	}
